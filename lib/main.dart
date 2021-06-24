0f2c4eb1e7110e04150b59c8262adcf0d9c696cd
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2021-06-24 15:16:28 +0300

    added state

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+class MyAppState extends State<MyApp>{ 
+  var questionIndex = 0; 
+
+  void answerQuestion (int index) { 
+    setState(() {
+      
+    questionIndex = questionIndex + 1 ;
+    });
+      print('question answered! $index' );
+  }
   @override
   Widget build(BuildContext context) {
     var questions = ['what\'s your favorite car', 'what\'s your favorite food'];
-    void answerQuestion (int index) { 
-        print('question answered! $index' );
-    }
+  
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app '),
         ),
         body: Column(children: [
-          Text('The question'),
+          Text(questions[questionIndex],),
           Row(children: [ ElevatedButton(
             child: Text('Answer 1'),
             onPressed: ()=>answerQuestion(1),
